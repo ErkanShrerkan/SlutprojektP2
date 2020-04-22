8a5cf3ee1500012a6bac274abb8d7cb839dbc6fc
ErkanShrerkan <ErkanShrerkan@users.noreply.github.com>
2020-04-22 23:40:33 +0200

    kommentar README

--- SlutprojektP2/SlutprojektP2/Program.cs
+++ SlutprojektP2/SlutprojektP2/Program.cs
@@ -10,6 +10,12 @@ namespace SlutprojektP2
     {
         static void Main(string[] args)
         {
+            // README
+            // konsollsettings för att spela spelet
+            // fönsterstorlek: bredd = 128, höjd 32
+            // bufferstorlek: bredd = 128, höjd >= 32
+            // storlek och teckensnitt kan vara vad som helst men jag föredrar...
+            // storlek = 36, teckensnitt = Consolas
             Game newGame = new Game(); // startar spelet
         }
     }
