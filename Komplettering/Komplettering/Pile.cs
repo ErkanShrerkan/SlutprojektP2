0f8896d936881c18153cbef0ba75724865c781dc
ErkanShrerkan <ErkanShrerkan@users.noreply.github.com>
2020-04-29 14:26:01 +0200

    walk done

---
+++ Komplettering/Komplettering/Pile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Komplettering
+{
+    class Pile<T>
+    {
+        T[] array;
+
+        public void Add(T item)
+        {
+
+        }
+
+        public int Count
+        {
+            get { return array.Length; }
+            set
+            {
+                array = new T[value];
+            }
+        }
+    }
+}
---
+++ Komplettering/Komplettering/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Komplettering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pile<int> newPile = new Pile<int>();
+            newPile.Count = 50;
+
+        }
+    }
+}
